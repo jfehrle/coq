ed7e180632a69980fb92c3e943229e0e20c5b2ae
Jim Fehrle <jim.fehrle@gmail.com>
2021-03-07 10:15:11 -0800

    Remove the :> type cast

--- theories/Program/Utils.v
+++ theories/Program/Utils.v
@@ -33,7 +33,7 @@ Notation " `  t " := (proj1_sig t) (at level 10, t at next level) : program_scop
 
 (** Coerces objects to their support before comparing them. *)
 
-Notation " x '`=' y " := ((x :>) = (y :>)) (at level 70) : program_scope.
+(* Notation " x '`=' y " := ((x :>) = (y :>)) (at level 70) : program_scope. *)
 
 Require Import Coq.Bool.Sumbool.
 
